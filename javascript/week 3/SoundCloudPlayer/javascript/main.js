beff88c288428608ac3398a9d4ecda16268f802a
craig - laptop <jones.craig94@gmail.com>
2019-04-11 22:05:35 +0100

    -

--- javascript/week 3/SoundCloudPlayer/javascript/main.js
+++ javascript/week 3/SoundCloudPlayer/javascript/main.js
@@ -9,17 +9,43 @@
 
 /* 2.  Query SoundCloud API */
 
+
+var SoundCloudAPI = {};
+
+SoundCloudAPI.initialize = function () {
+
  SC.initialize({
         client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
 });
 
+}
+
+SoundCloudAPI.init();
+
+SoundCloud.getTrack = function (inputValue) {
+
 // find all sounds of buskers licensed under 'creative commons share alike'
 
 SC.get('/tracks', {
-  q: 'buskers', 
+  q: inputValue, 
 }).then(function(tracks) {
   console.log(tracks);
 });
+
+}
+
+SoundCloudAPI.getTrack("Rilo Kiley");
+
+SoundCloudAPI.renderTracks = function () {
+
+	var card = document.createElement ('div');
+	card.classList.add("card");
+
+	var searchResults = document.querySelector (".js-search-results");
+	searchResults.appendChild(card);
+}
+
+SoundCloudAPI.renderTracks();
 
 
 
